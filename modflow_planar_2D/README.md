72f73d644d1b011d33c901b7afda762a1855f234
chenming <cmzhang1318@hotmail.com>
2022-03-23 10:46:56 +1000

    put the video link

---
+++ modflow_planar_2D/README.md
@@ -0,0 +1,5 @@
+### video tutorial to this script can be accessible from:
+
+ https://www.youtube.com/watch?v=33bVWu36miE&t=1s&ab_channel=ChenmingZhang
+
+ https://www.youtube.com/watch?v=gDAkmN4vy4Q&ab_channel=ChenmingZhang 
--- modflow_planar_2D/ewatering_mf6_jupytext.py
+++ modflow_planar_2D/ewatering_mf6_jupytext.py
@@ -33,6 +33,10 @@
 # also when using jupytxext, ensure there is no other texts after #%% as they will be
 # deleted.
 
+# a video tutorial to this script can be accessible from:
+# https://www.youtube.com/watch?v=33bVWu36miE&t=1s&ab_channel=ChenmingZhang
+# https://www.youtube.com/watch?v=gDAkmN4vy4Q&ab_channel=ChenmingZhang 
+
 
 # +
 #
